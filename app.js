bb22ae83090d9683067f9e5cd3c6ebc978852f03
Renan <renancf01@gmail.com>
2020-10-21 22:39:21 -0300

    add variables in controllers

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const app = require('express')();
 
-app.listen('3000', () => {
-    console.log('Online na porta 3000')
+app.get('/', (req, res) => {
+    let objeto = req.query
+    return res.send({message:"Tudo ok com o método GET"})
+});
+
+app.get('/', (req, res) => {
+    return res.send({message:"Tudo ok com o método POST"})
 })
+app.listen(3000, () => {
+    console.log('Online na porta 3000')
+});
+
+module.exports = app;
